f9d197ea898f0479744f2bffd11ed7adb8b57167
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2022-02-16 15:38:40 +0100

    adding new courses working, image src does not work

--- kurser.js
+++ kurser.js
@@ -56,3 +56,49 @@ fetch(jsonReq)
     }
     populateList(courses);
   });
+
+function addCourse() {
+  const courseNumber = document.getElementById("course-number").value;
+  const courseTitle = document.getElementById("course-title").value;
+  const courseInfo = document.getElementById("course-info").value;
+  const courseLength = document.getElementById("course-length").value;
+  const coursePrice = document.getElementById("course-price").value;
+  const courseImg = document.getElementById("btnAttachment").value;
+
+  let content = `
+    <div class="col">
+        <div class="card">
+          <img
+            src="${courseImg}"
+            class="card-img-top" alt="..." id="course-img" />
+          <div class="card-body">
+            <div class="shop-item">
+              <h5 class="shop-item-title">${courseTitle}</h5>
+              <p class="course-length"><b>${courseLength}</b></p>
+              <p class="card-text">
+                ${courseInfo}
+                <p class="shop-item-price">${coursePrice}</p>
+                <button type="button" class="btn btn-add btn-primary">
+                  Lägg i kundkorg
+                </button>
+              </p>
+            </div>
+          </div>
+        </div>
+        </div>
+        `;
+  document.getElementById("courses").innerHTML += content;
+  var addToCartButton = document.getElementsByClassName("btn-add");
+  for (var i = 0; i < addToCartButton.length; i++) {
+    var button = addToCartButton[i];
+    button.addEventListener("click", addToCart);
+  }
+}
+
+function openAttachment() {
+  document.getElementById("attachment").click();
+}
+
+function fileSelected(input) {
+  document.getElementById("btnAttachment").value = input.files[0].name;
+}
